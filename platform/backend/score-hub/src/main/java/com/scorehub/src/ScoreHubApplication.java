571f5316816d7006fbea6d831e726ec06320fafa
gauravshirkegs <shirkegaurav611@gmail.com>
2024-12-06 22:35:26 +0530

    backend spring boot project

---
+++ platform/backend/score-hub/src/main/java/com/scorehub/src/ScoreHubApplication.java
@@ -0,0 +1,13 @@
+package com.scorehub.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreHubApplication.class, args);
+	}
+
+}
---
+++ platform/backend/score-hub/src/test/java/com/scorehub/src/ScoreHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorehub.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
